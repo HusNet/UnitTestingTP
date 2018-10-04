dabac42a8aa709169e41175ef34703c72d48e211
R-Men <huser.gaetan@hotmail.com>
2018-10-04 15:50:36 +0200

    ImageTest start

---
+++ IED_UnitTest/ImageTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+using ImageEdgeDetection;
+
+namespace IED_UnitTest
+{
+    [TestClass]
+    public class ImageTest
+    {
+
+        private String imagePath = "Assets\\Images\\ImageTesting.jpg";
+        
+        private Bitmap bitmapImg = new Bitmap();
+
+        [TestMethod]
+        public void HellFilterTest(Bitmap bitmapImg)
+        {
+            
+
+
+        }
+    }
+}
