83045444a3a536da46c3f790f0f6f487ccc67ecb
aurelieglassey <aurelie.glassey@students.hevs.ch>
2018-10-04 15:20:13 +0200

    creation Unit Test

---
+++ IED_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IED_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("IED_UnitTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d0f2c22-1580-419f-83a4-03076da0aec4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- ImageEdgeDetection/Program.cs
+++ ImageEdgeDetection/Program.cs
@@ -10,6 +10,7 @@ namespace ImageEdgeDetection
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
         [STAThread]
         static void Main()
         {
